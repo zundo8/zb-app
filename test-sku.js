4cc985d09775cbef9fe1570967ef25046f3c925d
ztake8 <ztake8@gmail.com>
2026-05-05 02:36:37 +0530

    chore: production cleanup and synchronization

--- test-sku.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const p = await prisma.product.findFirst({
-    where: { OR: [ { sku: { contains: 'ZB26', mode: 'insensitive' } }, { barcode: { contains: 'ZB26', mode: 'insensitive' } } ] }
-  });
-  console.log(p);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
