21eae248af4abc8bc2d7aa84a9ebd2e7d29b7c35
ztake8 <ztake8@gmail.com>
2026-05-10 04:16:10 +0530

    fix: add missing KeyboardAvoidingView import

--- ZicaBella/src/components/payment/PaymentSheet.tsx
+++ ZicaBella/src/components/payment/PaymentSheet.tsx
@@ -14,6 +14,7 @@ import {
   Alert,
   Linking,
   Image,
+  KeyboardAvoidingView,
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
